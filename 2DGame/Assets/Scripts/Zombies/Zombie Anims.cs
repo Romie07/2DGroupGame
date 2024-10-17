d6a53b1a74e68e1da154747d66a95794a303df77
NekoZecho <zacharymunn07@gmail.com>
2024-10-17 10:30:34 -0700

    Zombie Visual Revamp

--- 2DGame/Assets/Scripts/Zombies/Zombie Anims.cs
+++ 2DGame/Assets/Scripts/Zombies/Zombie Anims.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class ZombieAnims : MonoBehaviour
 {
@@ -9,14 +10,17 @@ public class ZombieAnims : MonoBehaviour
     ZombieAI zAI;
     Vector3 dir;
     bool rotate = false;
-    GameObject shF;
-    GameObject shS;
+    Transform shF;
+    Transform shS;
 
     void Start()
     {
         zAI = GetComponent<ZombieAI>();
         anim = GetComponent<Animator>();
         spTr = GetComponent<Transform>();
+        shS = transform.Find("ShadowCaster Side");
+        shF = transform.Find("ShadowCaster Front");
+        shF.GetComponent<ShadowCaster2D>().enabled = false;
     }
 
     // Update is called once per frame
@@ -29,11 +33,25 @@ public class ZombieAnims : MonoBehaviour
         {
             transform.rotation = Quaternion.Euler (0, 0, 0);
             rotate = false;
+            shS.GetComponent<ShadowCaster2D>().enabled = true;
+            shF.GetComponent<ShadowCaster2D>().enabled = false;
         }
         else if (xDir > 0 && rotate == false)
         {
             transform.rotation = Quaternion.Euler(0, -180, 0);
             rotate = true;
+            shS.GetComponent<ShadowCaster2D>().enabled = true;
+            shF.GetComponent<ShadowCaster2D>().enabled = false;
+        }
+        if (yDir < 0)
+        {
+            shS.GetComponent<ShadowCaster2D>().enabled = false;
+            shF.GetComponent<ShadowCaster2D>().enabled = true;
+        }
+        else if (yDir > 0)
+        {
+            shS.GetComponent<ShadowCaster2D>().enabled = false;
+            shF.GetComponent<ShadowCaster2D>().enabled = true;
         }
         anim.SetFloat("x", xDir);
         anim.SetFloat("y", yDir);
