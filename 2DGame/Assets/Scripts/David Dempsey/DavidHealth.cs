29afd1627a1cd1c499cbd7981619ad33d632e97a
NekoZecho <zacharymunn07@gmail.com>
2024-10-25 09:54:39 -0700

    Fixed Zombie Shadows. Working on health of player and dmg of zombies.

--- 2DGame/Assets/Scripts/David Dempsey/DavidHealth.cs
+++ 2DGame/Assets/Scripts/David Dempsey/DavidHealth.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class DavidHealth : MonoBehaviour
 {
+    [Header("Health")]
     [SerializeField]
     float health = 3f;
     float maxHealth;
+    [Header("Regen")]
     [SerializeField]
     float regenSpeed = 2f;
     float regenAmount = 1f;
@@ -20,6 +22,9 @@ public class DavidHealth : MonoBehaviour
 
     void Update()
     {
-        
+        if (health <=0)
+        {
+            Debug.Log("You died");
+        }
     }
 }
--- 2DGame/Assets/Scripts/Enemies/Zombies/Zombie Anims.cs
+++ 2DGame/Assets/Scripts/Enemies/Zombies/Zombie Anims.cs
@@ -15,6 +15,11 @@ public class ZombieAnims : MonoBehaviour
     Transform frontShadow;
     ShadowCaster2D shF;
 
+    private const float targetX = 0f;
+    private const float firstTargetY = 1f;
+    private const float secondTargetY = -1f;
+    private const float tolerance = 0.5f;
+
     void Start()
     {
         zAI = GetComponent<ZombieAI>();
@@ -23,36 +28,58 @@ public class ZombieAnims : MonoBehaviour
         sideShadow = transform.Find("ShadowCaster Side");
         shS = sideShadow.GetComponent<ShadowCaster2D>();
         frontShadow = transform.Find("ShadowCaster Front");
-        shF = sideShadow.GetComponent<ShadowCaster2D>();
-        shF.enabled = false;
+        shF = frontShadow.GetComponent<ShadowCaster2D>();
     }
+
 
     // Update is called once per frame
     void Update()
-    { 
+    {
+        
         dir = zAI.getDir();
         float xDir = dir.x;
         float yDir = dir.y;
-        if (xDir < 0 && rotate == true)
+        Mathf.Abs(xDir);
+        if (IsInWalkingUpState() || IsInWalkingDownState())
+        {
+            verticalMovement();
+        }
+        else if (xDir < -0.5 && rotate == true)
         {
             transform.rotation = Quaternion.Euler (0, 0, 0);
             rotate = false;
             shS.enabled = true;
             shF.enabled = false;
         }
-        else if (xDir > 0 && rotate == false)
+        else if (xDir > 0.5 && rotate == false)
         {
             transform.rotation = Quaternion.Euler(0, -180, 0);
             rotate = true;
             shS.enabled = true;
             shF.enabled = false;
         }
-        if (yDir != 0 && spTr.)
-        {
-            shS.enabled = false;
-            shF.enabled = true;
-        }
         anim.SetFloat("x", xDir);
         anim.SetFloat("y", yDir);
+
+    }
+    private bool IsInWalkingUpState()
+    {
+        float x = anim.GetFloat("x");
+        float y = anim.GetFloat("y");  
+
+        return Mathf.Abs(x-targetX) < tolerance && Mathf.Abs(y-firstTargetY) < tolerance;
+    }
+    private bool IsInWalkingDownState()
+    {
+        float x = anim.GetFloat("x");
+        float y = anim.GetFloat("y");
+
+        return Mathf.Abs(x - targetX) < tolerance && Mathf.Abs(y - secondTargetY) < tolerance;
+    }
+
+    private void verticalMovement()
+    {
+        shS.enabled = false;
+        shF.enabled = true;
     }
 }
